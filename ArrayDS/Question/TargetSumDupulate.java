5b0629e88da4f39765256a5a53b30fa68b86d8aa
Saurav_RAICHAND <134069160+edgekallen@users.noreply.github.com>
2025-02-16 21:50:50 +0530

    Add files via upload

---
+++ ArrayDS/Question/TargetSumDupulate.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class TargetSumDupulate {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter Size of an array");
+        int size = scn.nextInt();
+        System.out.println("Enter Target");
+        int Target = scn.nextInt();
+        int[] arr = new int[size];
+        for (int i = 1; i < size; i++) {
+            arr[i] = scn.nextInt();
+        }
+        int result = getsum(arr,size,Target);
+        System.out.println("The Pair is:"+result);
+    }
+    public static int getsum(int arr[],int size,int Target){
+        int pair = 0;
+        for (int i=0; i <size; i++) {
+            for (int k =i+1; k<size; k++) {
+                if (arr[i] + arr[k] == Target) {
+                    pair++;
+                }
+            }
+        }
+
+        return pair;
+    }
+}
